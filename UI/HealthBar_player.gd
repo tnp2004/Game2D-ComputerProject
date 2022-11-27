78e694da3e223e059b80bdf2cbd201900b9c3906
tnp2004 <101316716+tnp2004@users.noreply.github.com>
2022-11-27 09:39:31 +0700

    Health Bar Update

--- UI/HealthBar_player.gd
+++ UI/HealthBar_player.gd
@@ -1,8 +1,31 @@
 extends Control
 
+var max_health = 0
+var current_health = 0
+
 func _ready():
-	$ProgressBar.max_value = get_parent().max_health
-	$ProgressBar.value = get_parent().max_health
+	max_health = get_parent().max_health
+	current_health = max_health
+	$ProgressBar.max_value = max_health
+	$ProgressBar.value = max_health
+	$ProgressBar2.max_value = max_health
+	$ProgressBar2.value = max_health
+	$ProgressBar/Label.text = str(current_health) + "/" + str(max_health)
+	set_up_icon_char()
+
+func set_up_icon_char():
+	if get_player_node():
+		var character = get_player_node().name
+		var char_icon = "res://charactersIcon/" + character + ".png"
+		$TextureRect2.texture = load(char_icon)
 
 func _on_health_updated(health):
 	$ProgressBar.value = health
+	$ProgressBar2.value = health
+	current_health = health
+	$ProgressBar/Label.text = str(current_health) + "/" + str(max_health)
+
+func get_player_node():
+	for i in get_tree().current_scene.get_children():
+		if i.is_in_group("player"):
+			return i # player node
